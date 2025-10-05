b48809456e1fc471e4651a0885299d89fe8326e7
ub122 <ulysseBottazzi@MacBook-Pro-5.local>
2025-10-05 22:53:42 +0100

    feat: ui setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>ShelfSense</h1>
+      <div className="card">
+        <p>
+          AI powered book recommandations based on your bookshelf !
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
